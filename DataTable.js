492692b496562394fd8caca1f61db69c9ea774f9
ankio <ankio@ankio.net>
2024-11-12 22:56:07 +0800

    feat:init

---
+++ DataTable.js
@@ -0,0 +1,322 @@
+class DataTable {
+  constructor(id) {
+    this.element = document.querySelector(id);
+
+    if (!this.element) {
+      console.error("Element not found");
+      return;
+    }
+
+    this.tableName = "table" + Math.random().toString(36).substring(2);
+
+    this.table = document.createElement("table");
+    this.table.id = this.tableName;
+    this.element.appendChild(this.table);
+    this.page = document.createElement("page-btn");
+    this.page.id = "page" + Math.random().toString(36).substring(2);
+    this.element.appendChild(this.page);
+  }
+
+  load(config) {
+    let that = this;
+    loader.load(
+      [
+        "/components/DataTable/Pagination.js",
+        "/components/DataTable/gridManager.js",
+        "/components/DataTable/datatable.css",
+        'URLUtils'
+      ],
+      function () {
+        that.config = Object.assign(
+          {
+            uri: "",
+            columns: [
+              {
+                field: "id",
+                name: "ID",
+                align: "center",
+                fixed: "left", //固定列，left为左边，right为右边,默认为空
+                width: "auto", //列宽度
+                sortable: true, //是否可排序
+                formatter: function (value, row, index) {
+                  return value;
+                },
+              },
+            ],
+            mobile: true,
+            selectable: true, //是否可以多选
+            lineHeight: "auto", //表格行高，默认"auto
+            height: "auto", //表格高度
+            events: {
+              onRowClick: function (row, rowIndex) {},
+              // row: 当前行数据
+              // rowIndex: 当前行索引
+              // colIndex: 当前列索引
+              onCellClick: function (row, rowIndex, colIndex, colName) {},
+              onPaged: function (page) {},
+              onSelect: function (rows) {},
+            },
+            empty_msg: "空空如也",
+            params: {},
+            headers: {},
+            pageSizes: [10, 20, 50, 100],
+            page: true,
+          },
+          config,
+        );
+
+        if (that.config.mobile) {
+          that.element.classList.add("table-mobile");
+        }
+
+        if (!that.config.page) {
+          that.page.style.display = "none";
+        }
+
+        that.data = [];
+
+        that.currentPage = $.url.getParam("page") || 1;
+        that.pageSize = $.url.getParam("size") || that.config.pageSizes[0];
+
+        that.totalRecords = 0;
+        that.sortColumn = "";
+        that.sortOrder = "";
+        that.loadData((loading) => {
+          try {
+            that.renderTable();
+            loading.close();
+          } catch (e) {
+            $.logger.error(e);
+          }
+        });
+      },
+    );
+  }
+
+  loadData(callback) {
+    this.selectData = [];
+    let that = this;
+    const loading = new Loading(this.element);
+    loading.show();
+    this.fetchData(() => {
+      callback(loading);
+    });
+  }
+
+  reload(params = undefined, reloadPage = false) {
+    this.config.params = params || this.config.params;
+    let that = this;
+    this.loadData((loading) => {
+      GridManager.setAjaxData(that.tableName, {
+        data: that.data,
+        total: that.totalRecords,
+      });
+      if (reloadPage && that.config.page) {
+        that.renderPage();
+      }
+      loading.close();
+    });
+  }
+
+  fetchData(callback) {
+    this.data = [];
+    this.totalRecords = 0;
+
+    const data = Object.assign(
+      {
+        page: this.currentPage,
+        pageSize: this.pageSize,
+        sortColumn: this.sortColumn,
+        sortOrder: this.sortOrder,
+      },
+      this.config.params,
+    );
+
+    $.ajax({
+      method: "GET",
+      url: this.config.uri,
+      headers: this.config.headers,
+      data: data,
+      success: (json) => {
+        try {
+          this.data = json.data;
+          this.totalRecords = json.count;
+        } catch (e) {
+          $.toaster.error("数据解析出错！");
+          console.error(e);
+        }
+      },
+      error: (xhr) => {
+        $.toaster.error("请求失败");
+        console.error(e);
+      },
+      complete: callback,
+    });
+  }
+
+  buildColumns() {
+    let columnData = [];
+    let leftFixedData = [];
+    let rightFixedData = [];
+    for (let col of this.config.columns) {
+      let width = col.width || null;
+      if (width === "auto") {
+        width = null;
+      }
+      if (
+          (col.fixed === "left" || col.fixed === "right") && width === null
+      ) {
+        width = 100;
+      }
+      let column = {
+        key: col.field,
+        text: col.name,
+        align: col.align,
+        fixed: col.fixed || undefined,
+        width: width,
+        sorting: col.sortable ? "" : undefined,
+        // 函数参数:
+        // cell: 当前单元格的渲染数据
+        // row: 当前单元格所在行的渲染数据, 本例中: 参数nodeData=== rowData.url
+        // index: 索引
+        // key: 列唯一键值
+        template: function (cell, row, index, key) {
+          let result = cell || "";
+          if (col.formatter) {
+            result = col.formatter(cell, row, index);
+          }
+          if (result === cell || (typeof result == "string" && result.indexOf("<") === -1)) {
+            result = `<mdui-tooltip>
+  <span>${result}</span>
+   <div slot="content">
+   ${result}
+  </div>
+</mdui-tooltip>`;
+          }
+
+          let text = col.name;
+          result = `<span class="table-mobile-name">${text}</span> <span class="table-col">${result}</span>`;
+
+          return result;
+        },
+      };
+
+
+      if (column.fixed === "left") {
+        leftFixedData.push(column);
+      } else if (column.fixed === "right") {
+        rightFixedData.push(column);
+      } else {
+        columnData.push(column);
+      }
+    }
+    return [...leftFixedData, ...columnData, ...rightFixedData];
+  }
+  renderPage() {
+    let that = this;
+    this.page.init({
+      pageIndex: 1,
+      pageSizes: that.config.pageSizes,
+      pageSize: that.pageSize,
+      currentPage: that.currentPage,
+      total: that.totalRecords,
+      //layout: "prev, pager, next, count, limits",
+      onPageChange: function (index, pageSize) {
+
+        that.currentPage = index;
+        that.pageSize = pageSize;
+
+        $.url.setParam("page", index);
+        $.url.setParam("size", pageSize);
+
+        that.reload(that.config.params);
+        if (that.config.events.onPaged)
+          that.config.events.onPaged(index, pageSize);
+      },
+    });
+  }
+
+  renderTable() {
+    let that = this;
+    this.table.GM({
+      gridManagerName: that.tableName,
+      supportAutoOrder: false,
+      height: that.config.height === "auto" ? undefined : that.config.height,
+      lineHeight:
+        that.config.height === "auto" ? undefined : that.config.lineHeight,
+      disableAutoLoading: true,
+      supportMenu: false,
+      supportAdjust: true,
+      supportDrag: false,
+      ajaxData: {
+        data: that.data,
+        total: that.totalRecords,
+      },
+      supportCheckbox: that.config.selectable,
+      checkboxConfig: {
+        // 选择列宽度配置
+        width: 40,
+
+        // 是否通过点击行来进行选中
+        useRowCheck: false,
+
+        // 当前选中操作是否使用单选
+        useRadio: false,
+
+        // 触发刷新类操作时(搜索、刷新、分页、排序、过滤)，是否禁用状态保持
+        disableStateKeep: undefined,
+
+        // 指定选中操作精准匹配字段，该值需保证每条数据的唯一性。默认不指定，对整条数据进行匹配。
+        // 在使用树型结构表格(supportTreeData)时，必须配置key，否则会造成已选中数据错误。
+        key: undefined, // 配置此项可提升选中操作性能, 数据量越大越明显。
+
+        // 复选时最大可选数，生效条件: supportCheckbox === true && useRadio === false
+        max: undefined,
+
+        // 是否使用固定列, 默认为undefined
+        // 接收两种值: 'left', 'right'
+        fixed: "left",
+      },
+      columnData: that.buildColumns(),
+      rowClick: function (row, rowIndex, tr) {
+        if (that.config.events.onRowClick)
+          that.config.events.onRowClick(row, rowIndex);
+      },
+      cellClick: function (row, rowIndex, colIndex, td) {
+        let tdName = td.getAttribute("td-name") || "";
+        if (that.config.events.onCellClick)
+          that.config.events.onCellClick(row, rowIndex, colIndex, tdName);
+      },
+      sortingBefore: function (sort) {
+        let key = Object.keys(sort);
+        if (key.length === 0) {
+          that.sortColumn = "";
+          that.sortOrder = "";
+          return;
+        }
+        that.sortColumn = key[0];
+        that.sortOrder = sort[key[0]].toLowerCase();
+        that.reload(that.config.params, true);
+      },
+      checkedAfter: function (checkedList, isChecked, rowData) {
+        that.selectData = checkedList;
+        if (that.config.events.onSelect)
+          that.config.events.onSelect(checkedList);
+      },
+    });
+    if (that.config.page) {
+      that.renderPage();
+    }
+  }
+
+  getSelectedRows() {
+    return this.selectData;
+  }
+  destroy() {
+    GridManager.destroy(this.tableName);
+  }
+
+  getRow(index) {
+    return this.data[index];
+  }
+}
---
+++ Pagination.js
@@ -0,0 +1,313 @@
+"use strict";
+
+class Pagination extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    this.shadowRoot.innerHTML = `
+            <div id="pagination" class="box page page-default"></div>
+            <style>
+                .box {
+                    display: flex;
+                    flex-direction: column; /* 改为列方向 */
+                    justify-content: center;
+                    align-items: center;
+                    flex-wrap: wrap;
+                    margin-top: 1rem;
+                }
+                .page-info{
+                    margin-top: 0.5rem;
+                }
+                .page-container {
+                    display: flex;
+                    align-items: center;
+                    flex-wrap: wrap;
+                }
+                .page-container-list {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    gap: 0.5rem; 
+                }
+                .page-curr {
+                    background-color: rgba(var(--mdui-color-primary))!important; 
+                    color: rgba(var(--mdui-color-on-primary)); 
+                    font-weight: bold; 
+                }
+                .page-prev, .page-next, .page-btn {
+                    padding: 0 10px;
+                    margin: 0 5px; 
+                }
+                .disabled {
+                    pointer-events: none;
+                    color: rgba(var(--mdui-color-on-surface-variant), 0.5);
+                }
+                .page-item {
+                    margin: 0.25rem;
+                    cursor: pointer;
+                    padding: 0.5rem;
+                    border-radius: var(--mdui-shape-corner-medium); 
+                    display: flex;
+                    justify-content: center;
+                    min-width: 1rem;
+                    background-color: rgba(var(--mdui-color-background)); 
+                    box-shadow: var(--mdui-elevation-level1);
+                    transition: background-color 0.3s, box-shadow 0.3s; 
+                }
+                .page-item:hover {
+                    background-color: rgba(var(--mdui-color-secondary-container)); 
+                    box-shadow: var(--mdui-elevation-level2);
+                }
+                .page-limits select {
+                    margin-left: 10px;
+                    padding: 0.25rem; 
+                    border-radius: var(--mdui-shape-corner-medium); 
+                    border: 1px solid rgba(var(--mdui-color-outline), 0.2); 
+                    background-color: rgba(var(--mdui-color-background)); 
+                    box-shadow: var(--mdui-elevation-level1);
+                }
+                .page-limits select:focus {
+                    border-color: rgba(var(--mdui-color-primary), 0.8); 
+                    box-shadow: var(--mdui-elevation-level2);
+                }
+                .page-count {
+                    font-size: 0.875rem; 
+                    color: rgba(var(--mdui-color-on-surface), 0.7); 
+                    padding: 0.5rem; 
+                }
+                @media (max-width: 600px) {
+                    .page-container-list {
+                        flex-wrap: wrap; 
+                        gap: 0.25rem; 
+                    }
+                    .page-item {
+                        min-width: 2rem; 
+                        padding: 0.25rem; 
+                    }
+                }
+            </style>
+        `;
+
+const that = this;
+
+    $.emitter.on('translate:set', () => {
+        TranslateUtils.autoTranslate(this.shadowRoot);
+    });
+  }
+
+  init(options) {
+    this._layout = ["first", "prev", "pager", "next", "last"];
+    this.options = Object.assign(
+      {
+        pageIndex: 1,
+        pageSize: 10,
+        pageSizes: [10, 20, 50, 100],
+        pageCount: 5,
+        total: 0,
+        layout: "first, prev, pager, next, last",
+        showCount: true, // 新增参数，控制是否显示条目数
+        showLimits: true, // 新增参数，控制是否显示分页数
+        onPageChange: function (index, pageSize) {},
+      },
+      options,
+    );
+
+    this.element = this.shadowRoot.getElementById("pagination");
+    this.pageNum = 0;
+    this.render();
+  }
+
+  render() {
+    this.pageNum = Math.ceil(this.options.total / this.options.pageSize);
+
+    if (this.options.pageIndex > this.pageNum)
+      this.options.pageIndex = this.pageNum;
+    if (this.options.pageIndex <= 0) this.options.pageIndex = 1;
+
+    this.element.innerHTML = ``;
+    const container = this.createElement("div", "page-container-list");
+
+    this.options.layout.split(",").forEach((v) => {
+      const method = v.trim();
+      if (this._layout.includes(method) && typeof this[method] === "function") {
+        const element = this[method]();
+        if (Array.isArray(element)) {
+          element.forEach((item) => container.appendChild(item));
+        } else if (element) container.appendChild(element);
+      }
+    });
+
+    this.element.appendChild(container);
+
+    // 创建第二行容器
+    if (this.options.showCount || this.options.showLimits) {
+      const infoContainer = this.createElement("div", [
+        "page-container-list",
+        "page-info",
+      ]);
+      if (this.options.showCount) {
+        const countElement = this.count();
+        infoContainer.appendChild(countElement);
+      }
+      if (this.options.showLimits) {
+        const limitsElement = this.limits();
+        infoContainer.appendChild(limitsElement);
+      }
+      this.element.appendChild(infoContainer);
+    }
+  }
+
+  first() {
+    const disabled = this.options.pageIndex <= 1 ? ["disabled"] : [];
+    const element = this.createElement("span", ["page-item", ...disabled]);
+    element.innerHTML = `<mdui-icon name="keyboard_double_arrow_left"></mdui-icon>`;
+    element.addEventListener("click", () => {
+      if (this.options.pageIndex > 1) {
+        this.handleChangePage(1);
+      }
+    });
+    return element;
+  }
+
+  prev() {
+    const disabled = this.options.pageIndex <= 1 ? ["disabled"] : [];
+    const element = this.createElement("span", ["page-item", ...disabled]);
+    element.innerHTML = `<mdui-icon name="chevron_left"></mdui-icon>`;
+    element.addEventListener("click", () => {
+      if (this.options.pageIndex > 1) {
+        this.handleChangePage(this.options.pageIndex - 1);
+      }
+    });
+    return element;
+  }
+
+  pager() {
+    const { min, max } = this.getBetween();
+    const arrs = this.generateArray(min, max);
+
+    let ret = [];
+    arrs.forEach((v) => {
+      let element = this.createElement("span", "page-item");
+      if (v === this.options.pageIndex) {
+        element.classList.add("page-curr");
+      }
+
+      element.innerText = v;
+
+      if (v !== this.options.pageIndex) {
+        element.addEventListener("click", () => {
+          if (v !== this.options.pageIndex) {
+            this.handleChangePage(v);
+          }
+        });
+      }
+      ret.push(element);
+    });
+
+    return ret;
+  }
+
+  next() {
+    const disabled = this.options.pageIndex >= this.pageNum ? ["disabled"] : [];
+    const element = this.createElement("span", ["page-item", ...disabled]);
+    element.innerHTML = `<mdui-icon name="chevron_right"></mdui-icon>`;
+    element.addEventListener("click", () => {
+      if (this.options.pageIndex < this.pageNum) {
+        this.handleChangePage(this.options.pageIndex + 1);
+      }
+    });
+    return element;
+  }
+
+  last() {
+    const disabled = this.options.pageIndex >= this.pageNum ? ["disabled"] : [];
+    const element = this.createElement("span", ["page-item", ...disabled]);
+    element.innerHTML = `<mdui-icon name="keyboard_double_arrow_right"></mdui-icon>`;
+    element.addEventListener("click", () => {
+      if (this.options.pageIndex < this.pageNum) {
+        this.handleChangePage(this.pageNum);
+      }
+    });
+    return element;
+  }
+
+  count() {
+    const span = this.createElement("span", "page-count");
+    span.innerText = `共 ${this.options.total} 条`;
+    return span;
+  }
+
+  limits() {
+    const span = this.createElement("mdui-dropdown", "page-limits");
+    const button = this.createElement("mdui-button");
+    button.setAttribute("slot", "trigger");
+    button.setAttribute("variant", "text");
+    button.innerText = `${this.options.pageSize} 条/页`;
+    span.appendChild(button);
+
+    const select = this.createElement("mdui-menu", "");
+    select.setAttribute("selects", "single");
+    select.setAttribute("value", `${this.options.pageSizes[0]}`);
+    this.options.pageSizes.forEach((size) => {
+      const option = this.createElement("mdui-menu-item", "");
+      option.value = size;
+      option.innerText = `${size} 条/页`;
+      select.appendChild(option);
+    });
+
+    select.addEventListener("change", (event) => {
+      this.options.pageSize = parseInt(event.target.value);
+      this.handleChangePage(1);
+    });
+
+    span.appendChild(select);
+    return span;
+  }
+
+  handleChangePage(index) {
+    this.options.pageIndex = index;
+    if (typeof this.options.onPageChange === "function") {
+      this.options.onPageChange(index, this.options.pageSize);
+    }
+    this.render();
+  }
+
+  getBetween() {
+    const half = Math.floor(this.options.pageCount / 2);
+    let min = this.options.pageIndex - half;
+    let max = this.options.pageIndex + half;
+
+    if (min < 1) {
+      min = 1;
+      max = Math.min(this.pageNum, this.options.pageCount);
+    }
+
+    if (max > this.pageNum) {
+      max = this.pageNum;
+      min = Math.max(1, this.pageNum - this.options.pageCount + 1);
+    }
+
+    return { min, max };
+  }
+
+  generateArray(start, end) {
+    const arr = [];
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  createElement(tag, classList) {
+    const el = document.createElement(tag);
+    if (Array.isArray(classList)) {
+      classList.forEach((cls) => el.classList.add(cls));
+    } else if (classList) {
+      el.classList.add(classList);
+    }
+    return el;
+  }
+}
+
+customElements.define("mdui-page-btn", Pagination);
